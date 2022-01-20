7f9bbb0c66f87310cef6c656d47e62c46dbe8492
Vincenzo Esposito <vncenzo.esposito@voismart.it>
2022-01-20 17:52:39 +0100

    remove background thread on SipService - #147

--- sipservice/src/main/java/net/gotev/sipservice/BackgroundService.java
+++ sipservice/src/main/java/net/gotev/sipservice/BackgroundService.java
@@ -3,7 +3,6 @@ package net.gotev.sipservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.*;
-import android.os.Process;
 
 /**
  * Service with a background worker thread.
@@ -11,23 +10,14 @@ import android.os.Process;
  */
 class BackgroundService extends Service {
 
-    private HandlerThread mWorkerThread;
     private Handler mHandler;
     private PowerManager.WakeLock mWakeLock;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
-        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, getClass().getSimpleName());
-
-        mWakeLock.acquire();
-
-        mWorkerThread = new HandlerThread(getClass().getSimpleName(), Process.THREAD_PRIORITY_FOREGROUND);
-        mWorkerThread.setPriority(Thread.MAX_PRIORITY);
-        mWorkerThread.start();
-        mHandler = new Handler(mWorkerThread.getLooper());
+        mHandler = new Handler(Looper.getMainLooper());
+        acquireWakeLock();
     }
 
     @Override
@@ -38,8 +28,7 @@ class BackgroundService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mWorkerThread.quitSafely();
-        mWakeLock.release();
+        releaseWakeLock();
     }
 
     protected void enqueueJob(Runnable job) {
@@ -52,5 +41,17 @@ class BackgroundService extends Service {
 
     protected void dequeueJob(Runnable job) {
         mHandler.removeCallbacks(job);
+    }
+
+    public void acquireWakeLock() {
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, getClass().getSimpleName());
+        mWakeLock.acquire();
+    }
+
+    public void releaseWakeLock() {
+        if (mWakeLock != null && mWakeLock.isHeld()) {
+            mWakeLock.release();
+        }
     }
 }
